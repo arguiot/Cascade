1f908ad1715e246929d34f58a41eb1f02a27bbc3
Arthur Guiot <arguiot@gmail.com>
2018-09-25 19:31:23 +0800

    Added mixin caller

--- src/functions/mixin.js
+++ src/functions/mixin.js
@@ -1,3 +1,7 @@
 mixin(name) {
-	if (this.c.mixins.hasOwnProperty)
+	if (this.c.mixins.hasOwnProperty(name)) {
+		return this.c.mixins[name](this)
+	}
+	throw "[Cascade] Mixins: wrong name"
+	return this
 }
