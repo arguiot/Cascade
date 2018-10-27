c573cbfb3e464a5404b1f4e5a5757822200da92d
Arthur Guiot <arguiot@gmail.com>
2018-10-27 12:58:54 +0800

    Added position

---
+++ src/functions/position.js
@@ -0,0 +1,24 @@
+position(opt) {
+	this._addProp("position", opt)
+	return this
+}
+
+right(opt) {
+	this._addProp("right", opt)
+	return this
+}
+
+left(opt) {
+	this._addProp("left", opt)
+	return this
+}
+
+top(opt) {
+	this._addProp("top", opt)
+	return this
+}
+
+bottom(opt) {
+	this._addProp("bottom", opt)
+	return this
+}
