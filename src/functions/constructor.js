081470c9d181892b8966421bcc82c6685a668e93
Arthur Guiot <arguiot@gmail.com>
2018-10-28 10:34:28 +0800

    Implemented comma style selector

--- src/functions/constructor.js
+++ src/functions/constructor.js
@@ -1,4 +1,7 @@
-constructor(selector, cascade) {
+constructor() {
+	const args = [...arguments]
+	const cascade = args.pop()
+	const selector = args.join(",")
 	this.c = cascade
 	this.s = selector
 }
